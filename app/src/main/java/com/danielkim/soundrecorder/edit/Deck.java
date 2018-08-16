11cc3747acc2c1e0a252139fb016c8454e0d062c
Patrick Conner <PConner000@gmail.com>
2018-08-16 15:34:41 -0600

    Implemented (untested) sample getters in Deck

--- app/src/main/java/com/danielkim/soundrecorder/edit/Deck.java
+++ app/src/main/java/com/danielkim/soundrecorder/edit/Deck.java
@@ -4,8 +4,11 @@ import com.danielkim.soundrecorder.edit.events.Event;
 import com.danielkim.soundrecorder.edit.events.EventHandler;
 import com.danielkim.soundrecorder.edit.exceptions.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck implements AudioProvider, EventHandler {
-    private Channel[] data;
+    private List<Channel> data = new ArrayList<>();
 
     public void render(String fileName) {
         throw new NotImplementedException();
@@ -18,12 +21,30 @@ public class Deck implements AudioProvider, EventHandler {
 
     @Override
     public float getSample(long sampleIndex) {
-        throw new NotImplementedException();
+        float ret = 0;
+        for(Channel c: data){
+            ret += c.getSample(sampleIndex);
+        }
+        return ret;
     }
 
     @Override
     public long getSamples(long startSampleIndex, float[] returnedSamples) {
-        throw new NotImplementedException();
+        for(int i = 0; i < returnedSamples.length; i++){
+            returnedSamples[i] = 0;
+        }
+        long length = 0;
+        float[] tmp = new float[returnedSamples.length];
+        for(Channel c: data){
+            long l = c.getSamples(startSampleIndex, tmp);
+            for(int i = 0; i < l; i++){
+                returnedSamples[i] += tmp[i];
+            }
+            if(l > length){
+                length = l;
+            }
+        }
+        return length;
     }
 
 
