971b9fead34ceafb4be59140488cfe4e97361a46
Ramon Caballero <ramon.caballeroiv@gmail.com>
2018-08-16 15:30:33 -0600

    Stubbed out Deck tests.

---
+++ app/src/test/java/com/danielkim/soundrecorder/edit/DeckTest.java
@@ -0,0 +1,24 @@
+package com.danielkim.soundrecorder.edit;
+
+import junit.framework.TestCase;
+
+public class DeckTest extends TestCase {
+	
+	public void testRender() {
+	}
+	
+	public void testHandleEvent() {
+	}
+	
+	public void testGetSample() {
+	}
+	
+	public void testGetSamples() {
+	}
+	
+	public void testGetSampleRate() {
+	}
+	
+	public void testSetSampleRate() {
+	}
+}
