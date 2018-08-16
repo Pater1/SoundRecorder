bb4ec30fde5716d2d2fddd7cc07ac9d68dba3ed0
Patrick Conner <PConner000@gmail.com>
2018-08-16 15:20:39 -0600

    Implementented (untested) sample get logic for Channel

--- app/src/main/java/com/danielkim/soundrecorder/edit/Channel.java
+++ app/src/main/java/com/danielkim/soundrecorder/edit/Channel.java
@@ -4,19 +4,33 @@ import com.danielkim.soundrecorder.edit.events.Event;
 import com.danielkim.soundrecorder.edit.events.EventHandler;
 import com.danielkim.soundrecorder.edit.exceptions.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Channel implements AudioProvider, EventHandler {
-	
-	private AudioChunk[] data;
+	private List<AudioChunk> data = new ArrayList<>();
 	private ChannelHeader header = new ChannelHeader();
 
 	@Override
 	public float getSample(long sampleIndex) {
-		throw new NotImplementedException();
+		for(AudioChunk c: data){
+			float l = c.getSample(sampleIndex - c.getStartIndex());
+			if(Math.abs(l) < 1E-5){
+				return l;
+			}
+		}
+		return 0;
 	}
 
 	@Override
 	public long getSamples(long startSampleIndex, float[] returnedSamples) {
-		throw new NotImplementedException();
+		for(AudioChunk c: data){
+			long l = c.getSamples(startSampleIndex - c.getStartIndex(), returnedSamples);
+			if(l > 0){
+				return l;
+			}
+		}
+		return 0;
 	}
 
 	private long sampleRate;
