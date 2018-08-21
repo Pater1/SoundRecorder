ed12c6347bbd9434545a4ab65e90f655931fb13a
Patrick Conner <PConner000@gmail.com>
2018-08-20 18:58:07 -0600

    Audio chunk generator helper in AudioChunkInMemoryTest

--- app/src/test/java/com/danielkim/soundrecorder/edit/AudioChunkInMemoryTest.java
+++ app/src/test/java/com/danielkim/soundrecorder/edit/AudioChunkInMemoryTest.java
@@ -2,12 +2,26 @@ package com.danielkim.soundrecorder.edit;
 
 import junit.framework.TestCase;
 
+import java.util.Random;
+
 public class AudioChunkInMemoryTest extends TestCase {
+	private static final Random rand = new Random();
+	private AudioChunk buildChunkInMemory(float[] memory) {
+		float[] rawSamples = new float[rand.nextInt()];
+		for (int i = 0; i < rawSamples.length; i++) {
+			rawSamples[i] = rand.nextFloat();
+		}
+
+		return new AudioChunkInMemory(rawSamples);
+	}
 	
 	public void testGetSample() {
+
 	}
 	
 	public void testGetSamples() {
+
+
 	}
 	
 	public void testPassdownHandleEvent() {
