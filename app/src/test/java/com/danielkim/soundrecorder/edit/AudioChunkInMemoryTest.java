0430da82b0080962e57757979f2f25c1dcad4ff3
Ramon Caballero <ramon.caballeroiv@gmail.com>
2018-08-20 19:42:26 -0600

    Added dependency for junit tests. Implemented testGetSampleInBounds and testGetSampleOutOfBounds.

--- app/src/test/java/com/danielkim/soundrecorder/edit/AudioChunkInMemoryTest.java
+++ app/src/test/java/com/danielkim/soundrecorder/edit/AudioChunkInMemoryTest.java
@@ -1,5 +1,6 @@
 package com.danielkim.soundrecorder.edit;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import java.util.Random;
@@ -13,8 +14,21 @@ public class AudioChunkInMemoryTest extends TestCase {
 		return new AudioChunkInMemory(memory);
 	}
 	
-	public void testGetSample() {
-
+	public void testGetSampleInBounds() {
+		float[] memory = new float[rand.nextInt(100)];
+		AudioChunk chunk = buildChunkInMemory(memory);
+		
+		int index = rand.nextInt(memory.length);
+		assertEquals(memory[index], chunk.getSample(index));
+	}
+	
+	public void testGetSampleOutOfBounds() {
+		final float TOLERANCE = (float) 0.000001;
+		float[] memory = new float[rand.nextInt(100)];
+		AudioChunk chunk = buildChunkInMemory(memory);
+		
+		assertTrue(0.0 - chunk.getSample(-1) < TOLERANCE);
+		assertTrue(0.0 - chunk.getSample(memory.length) < TOLERANCE);
 	}
 	
 	public void testGetSamples() {
