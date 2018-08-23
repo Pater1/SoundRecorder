dd6516b84843e45c41a8828e484faed07c8cf2bf
Ramon Caballero <ramon.caballeroiv@gmail.com>
2018-08-22 21:41:28 -0600

    Finished the displaying of a chunk, displaying a single cursor works, no support for 2 cursors yet.

--- app/src/main/java/com/danielkim/soundrecorder/edit/canvases/AudioChunkCanvas.java
+++ app/src/main/java/com/danielkim/soundrecorder/edit/canvases/AudioChunkCanvas.java
@@ -1,72 +1,146 @@
 package com.danielkim.soundrecorder.edit.canvases;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.os.OperationCanceledException;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.danielkim.soundrecorder.R;
 import com.danielkim.soundrecorder.edit.AudioChunk;
-import com.danielkim.soundrecorder.edit.exceptions.NotImplementedException;
 
+/* In order for this view to work, it needs an AudioChunk to render, thus we have to create this
+programmatically.
+ */
+@SuppressLint("ViewConstructor")
 public class AudioChunkCanvas extends View {
 	
 	private static final int GAP = 4;
 	private static final int SCALE = 1;
+	private static final int TOLERANCE = 20;
 	
 	private AudioChunk chunk;
 	private Bitmap mBitmap;
 	private Canvas mCanvas;
 	private Path mPath;
-	private Paint mPaint;
-	private int startHighlight, endHighlight;
+	private Paint chunkPaint;
+	private Paint cursorPaint;
+	private float startCursor, endCursor;
 	
 	Context context;
 	
 	public AudioChunkCanvas(Context context, AudioChunk chunk) {
 		super(context);
+		this.context = context;
 		
 		mPath = new Path();
 		
-		mPaint = new Paint();
-		mPaint.setAntiAlias(true);
-		mPaint.setColor(getResources().getColor(R.color.primary));
-		mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-		mPaint.setStrokeJoin(Paint.Join.ROUND);
-		mPaint.setStrokeWidth(4f);
+		chunkPaint = new Paint();
+		chunkPaint.setAntiAlias(true);
+		chunkPaint.setColor(getResources().getColor(R.color.primary));
+		chunkPaint.setStyle(Paint.Style.STROKE);
+		chunkPaint.setStrokeJoin(Paint.Join.ROUND);
+		chunkPaint.setStrokeWidth(4f);
+		
+		cursorPaint = new Paint();
+		cursorPaint.setAntiAlias(true);
+		cursorPaint.setColor(getResources().getColor(R.color.cursor));
+		cursorPaint.setStyle(Paint.Style.STROKE);
+		cursorPaint.setAlpha(150);
+		cursorPaint.setStrokeWidth(4f);
 		
 		this.chunk = chunk;
-		drawChunk();
 	}
 	
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		
-		mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-		mCanvas = new Canvas(mBitmap);
+		Toast.makeText(context, "Width: " + getWidth() + " Height: " + getHeight(), Toast.LENGTH_SHORT).show();
+		if (w != 0 && h != 0) {
+			mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+			mCanvas = new Canvas(mBitmap);
+			
+			genChunkPath(0, w / GAP);
+			setSingleCursor(w / (GAP * 2));
+		}
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		
-		canvas.drawPath(mPath, mPaint);
+		canvas.drawPath(mPath, chunkPaint);
+		
+		float end = Math.max(endCursor, startCursor + GAP);
+		canvas.drawRect(startCursor, 0, end, getHeight(), cursorPaint);
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		throw new NotImplementedException();
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				if (isSingleCursor()) {
+					moveSingleCursor(event.getX());
+				}
+				break;
+			
+			case MotionEvent.ACTION_MOVE:
+				if (isSingleCursor()) {
+					moveSingleCursor(event.getX());
+				} else {
+					moveClosestCursor(event.getX());
+				}
+				break;
+			
+			case MotionEvent.ACTION_UP:
+				tryMergeCursors();
+				break;
+		}
+		invalidate();
+		
+		return true;
 	}
 	
-	public void drawChunk() {
-		mPath.reset();
-		float prevX = 0, prevY = 0;
+	public void moveClosestCursor(float touchX) {
+		float distanceToStart = Math.abs(touchX - startCursor);
+		float distanceToEnd = Math.abs(touchX - endCursor);
 		
-		for (int i = 0; i < chunk.getLength(); i++) {
+		if (distanceToStart <= TOLERANCE || distanceToEnd <= TOLERANCE) {
+			if (distanceToStart < distanceToEnd) {
+				startCursor = touchX;
+			} else {
+				endCursor = touchX;
+			}
+		}
+	}
+	
+	public void moveSingleCursor(float newX) {
+		if (Math.abs(newX - getCursor()) <= TOLERANCE) {
+			startCursor = endCursor = newX;
+		}
+	}
+	
+	public boolean isSingleCursor() {
+		return startCursor == endCursor;
+	}
+	
+	public void tryMergeCursors() {
+		if (Math.abs(startCursor - endCursor) <= TOLERANCE) {
+			startCursor = endCursor;
+		}
+	}
+	
+	public void genChunkPath(int startIndex, int endIndex) {
+		mPath.reset();
+		float prevX = startIndex, prevY = chunk.getSample(endIndex);
+		endIndex = Math.min(endIndex, getWidth() / GAP);
+		
+		for (int i = startIndex; i <= endIndex; i++) {
 			float curX = GAP * i;
 			float curY = chunk.getSample(i) * SCALE;
 			
@@ -75,5 +149,17 @@ public class AudioChunkCanvas extends View {
 			prevX = curX;
 			prevY = curY;
 		}
+	}
+	
+	public float getCursor() {
+		if (!isSingleCursor()) {
+			throw new OperationCanceledException("there are 2 cursors present");
+		}
+		
+		return startCursor;
+	}
+	
+	public void setSingleCursor(float cursor) {
+		startCursor = endCursor = cursor;
 	}
 }
