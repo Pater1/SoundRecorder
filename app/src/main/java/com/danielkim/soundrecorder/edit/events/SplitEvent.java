ac2031f47e7e58b5c5995fba04b7b7342e1be937
Patrick Conner <PConner000@gmail.com>
2018-08-25 14:02:40 -0600

    Fixed SplitEvent compiler error

--- app/src/main/java/com/danielkim/soundrecorder/edit/events/SplitEvent.java
+++ app/src/main/java/com/danielkim/soundrecorder/edit/events/SplitEvent.java
@@ -7,12 +7,9 @@ import com.danielkim.soundrecorder.edit.exceptions.NotImplementedException;
 
 public class SplitEvent extends Event {
     public SplitEvent(long effectSampleIndex, int effectChannel) {
-        super(effectChannel, EffectTarget.CHANNEL | EffectTarget.CHUNK);
-
-        this.effectSampleIndex = effectSampleIndex;
+        super(effectSampleIndex, effectSampleIndex, effectChannel, EffectTarget.CHANNEL | EffectTarget.CHUNK);
     }
 
-    private long effectSampleIndex;
     private Channel inChannel = null;
 
     @Override
@@ -20,8 +17,8 @@ public class SplitEvent extends Event {
         if(EffectTarget.isTargeted(this.getTargetFlags(), handler.getTargetedFlag())){
             if(handler instanceof AudioChunk){
                 AudioChunk original = (AudioChunk)handler;
-                AudioChunk pre = new SubChunk(original, 0, effectSampleIndex);
-                AudioChunk post = new SubChunk(original, effectSampleIndex, original.getLength());
+                AudioChunk pre = new SubChunk(original, 0, getEffectStartSampleIndex());
+                AudioChunk post = new SubChunk(original, getEffectStartSampleIndex(), original.getLength());
 
                 inChannel.remove(original);
                 inChannel.add(pre);
