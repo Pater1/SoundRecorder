45e21a0101c0fa83566638ad59298e5534a4a831
Julie <jbabylon@student.neumont.edu>
2018-08-27 17:50:30 -0600

    Readded missing code from Option, MergeOption and SplitTrimOption

--- app/src/main/java/com/danielkim/soundrecorder/edit/editingoptions/Option.java
+++ app/src/main/java/com/danielkim/soundrecorder/edit/editingoptions/Option.java
@@ -1,5 +1,43 @@
 package com.danielkim.soundrecorder.edit.editingoptions;
 
+import android.graphics.Color;
+
 public abstract class Option {
-    public abstract boolean execute();
+    protected abstract boolean passedDownOnTouchUp(long[] cursorArray, int channelIndex);
+    protected abstract boolean passedDownOnTouchMove(long[] cursorArray, int channelIndex);
+    protected abstract boolean passedDownOnTouchDown(long[] cursorArray, int channelIndex);
+    public abstract int getColor();
+
+    private Thread onTouchMove;
+    private boolean isUpdating;
+    private long[] cursorArray;
+    private int channelIndex;
+
+    public Option() {
+        this.onTouchMove = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (isUpdating) {
+                    passedDownOnTouchMove(cursorArray, channelIndex);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    public final boolean onTouchUp(long[] cursorArray, int channelIndex) {
+        this.isUpdating = false;
+        return passedDownOnTouchUp(cursorArray, channelIndex);
+    }
+    public final boolean onTouchDown(long[] cursorArray, int channelIndex) {
+        this.channelIndex = channelIndex;
+        this.cursorArray = cursorArray;
+        this.isUpdating = true;
+        this.onTouchMove.start();
+        return passedDownOnTouchDown(cursorArray, channelIndex);
+    }
 }
