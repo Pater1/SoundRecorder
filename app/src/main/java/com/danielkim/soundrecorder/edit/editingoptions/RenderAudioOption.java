553f20948190fb232e6cd47d56ff82704560b182
Ramon Caballero <ramon.caballeroiv@gmail.com>
2018-08-27 23:22:43 -0600

    Made option to render the deck

---
+++ app/src/main/java/com/danielkim/soundrecorder/edit/editingoptions/RenderAudioOption.java
@@ -0,0 +1,76 @@
+package com.danielkim.soundrecorder.edit.editingoptions;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import com.danielkim.soundrecorder.R;
+import com.danielkim.soundrecorder.edit.fragments.DeckFragment;
+
+public class RenderAudioOption extends Option {
+	
+	private DeckFragment deckFragment;
+	
+	public RenderAudioOption(DeckFragment deckFragment) {
+		this.deckFragment = deckFragment;
+	}
+	
+	@Override
+	protected boolean passedDownOnTouchUp(long[] cursorArray, int channelIndex) {
+		Context context = deckFragment.getActivity();
+		
+		AlertDialog.Builder renameFileBuilder = new AlertDialog.Builder(context);
+		
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View view = inflater.inflate(R.layout.dialog_rename_file, null);
+		
+		final EditText input = (EditText) view.findViewById(R.id.new_name);
+		
+		renameFileBuilder.setTitle("Render");
+		renameFileBuilder.setCancelable(true);
+		renameFileBuilder.setPositiveButton(context.getString(R.string.dialog_action_ok),
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						try {
+							String value = input.getText().toString().trim() + ".wav";
+							deckFragment.renderAudio(value);
+						} catch (Exception e) {
+//							Log.e(LOG_TAG, "exception", e);
+						}
+						
+						dialog.cancel();
+					}
+				});
+		renameFileBuilder.setNegativeButton(context.getString(R.string.dialog_action_cancel),
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		
+		renameFileBuilder.setView(view);
+		AlertDialog alert = renameFileBuilder.create();
+		alert.show();
+		return true;
+	}
+	
+	@Override
+	protected boolean passedDownOnTouchMove(long[] cursorArray, int channelIndex) {
+		return false;
+	}
+	
+	@Override
+	protected boolean passedDownOnTouchDown(long[] cursorArray, int channelIndex) {
+		return false;
+	}
+	
+	@Override
+	public int getColor() {
+		return Color.CYAN;
+	}
+}
